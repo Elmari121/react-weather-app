7d28d2a910294e5ada86434a9e9f8b88756c0541
Elmari121 <robbertselmari@gmail.com>
2025-05-19 14:27:17 +0200

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 export default function App() {return (
 
     <div className="App">
+      <div className="container"> 
       <h1>Weather App</h1>
       
 
 <footer>
 This project was created by Elmari Robberts and is open-sourced on 
-<a href="https://github.com/Elmari121/react-weather-app" target="_blank" rel="noopener noreferrer"> GitHub.</a>
+<a href="https://github.com/Elmari121/react-weather-app" target="_blank" rel="noopener noreferrer"> GitHub.</a> 
 and hosted on <a href="https://bejewelled-pony-7f1ece.netlify.app/" target="_blank" rel="noopener noreferrer"> Netlify.</a>
     </footer>
-    
+    </div>
 
     </div>
   );
