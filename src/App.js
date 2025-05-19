fbcd9dce883b8e6f41b7d31124dc74eafe7f1e67
Elmari121 <robbertselmari@gmail.com>
2025-05-19 08:02:05 +0200

    add footer github and netlify link remove unnesesery content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-
+import React from 'react';
 import './App.css';
+export default function App() {return (
 
-function App() {
-  return (
     <div className="App">
-      <header className="App-header">
+      <h1>Weather App</h1>
+      
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<footer>
+This project was created by Elmari Robberts and is open-sourced on 
+<a href="https://github.com/Elmari121/react-weather-app" target="_blank" rel="noopener noreferrer"> GitHub.</a>
+and hosted on <a href="https://bejewelled-pony-7f1ece.netlify.app/" target="_blank" rel="noopener noreferrer"> Netlify.</a>
+    </footer>
+    
+
     </div>
   );
 }
 
-export default App;
+
